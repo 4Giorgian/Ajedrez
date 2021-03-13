09ccfa058d034dc3537a9f38c04bd35ef31d1a9a
nicoperu <nicolas.masias@unmsm.edu.pe>
2021-03-13 07:11:59 -0500

    nuevo push

--- src/ChessGamePieceMoves.java
+++ src/ChessGamePieceMoves.java
@@ -5,6 +5,7 @@ import javax.swing.ImageIcon;
 
 public interface ChessGamePieceMoves {    
     
+    public ArrayList<String> calculatePossibleMoves( ChessGameBoard board );
     public ArrayList<String> calculateSouthMoves(ChessGameBoard board,int numMoves );
     public ArrayList<String> calculateNorthMoves(ChessGameBoard board, int numMoves );
     public ArrayList<String> calculateEastMoves(ChessGameBoard board, int numMoves );
@@ -13,6 +14,12 @@ public interface ChessGamePieceMoves {
     public ArrayList<String> calculateNorthEastMoves(ChessGameBoard board, int numMoves );
     public ArrayList<String> calculateSouthWestMoves(ChessGameBoard board, int numMoves );
     public ArrayList<String> calculateSouthEastMoves(ChessGameBoard board, int numMoves ); 
+    public boolean move( ChessGameBoard board, int row, int col );
+    public boolean canMove( ChessGameBoard board, int row, int col );
+    public boolean testMoveForKingSafety(ChessGameBoard board, int row, int col);
+    public void updatePossibleMoves( ChessGameBoard board );
+    public void showLegalMoves( ChessGameBoard board );
+    public boolean hasLegalMoves( ChessGameBoard board );
 }
 
     
