2427fc8946866c03975aad6856901905b9843381
nicoperu <nicolas.masias@unmsm.edu.pe>
2021-03-14 20:29:51 -0500

    Prototipo

--- src/IRook.java
+++ src/IRook.java
@@ -1,13 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author niquito
  */
-public class IRook {
-    
+import javax.swing.ImageIcon;
+
+
+public interface IRook  extends Cloneable {
+  
+    public ImageIcon createWhiteRook();
+    public ImageIcon createBlackRook();
+    IRook clonar();
+  
 }
