db04671aa97ec697b08e9ef97ef993328c614f18
nicoperu <nicolas.masias@unmsm.edu.pe>
2021-04-03 03:40:17 -0500

    Patron Decorator

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
 
 /**
  *
  * @author niquito
  */
-public class ChessPiece {
-    
+public interface ChessPiece {
+    public ArrayList<String> calculatePossibleMoves(ChessGameBoard board);
+    public ImageIcon createImageByPieceType();
 }
