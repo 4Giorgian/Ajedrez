780457c2872eb94adb66539bc55ae06ee73e9532
nicoperu <nicolas.masias@unmsm.edu.pe>
2021-04-03 03:30:06 -0500

    Factory Method

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,11 +1,13 @@
-import java.util.ArrayList;
-import javax.swing.ImageIcon;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 /**
  *
  * @author niquito
  */
-public interface ChessPiece {
-    public ArrayList<String> calculatePossibleMoves(ChessGameBoard board);
-    public ImageIcon createImageByPieceType();
+public class ChessPiece {
+    
 }
