9afa601b1266a2e77a1d551557c6a0dd801b004d
raulperez4 <73408590+raulperez4@users.noreply.github.com>
2021-04-17 09:31:39 -0500

    Patron  Strategy- 1.5

--- src/chess/ChessMain.java
+++ src/chess/ChessMain.java
@@ -21,13 +21,15 @@ public class ChessMain{
      
      
     public static void main( String[] args ){
-        
+        /*
         JFrame frame = new JFrame( "YetAnotherChessGame 1.0" );
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         frame.getContentPane().add (ChessPanel.getInstance());
         frame.pack();
         frame.setVisible( true );
         System.out.println("Hola"); 
-  
+    } */
+    
+   FacadeChess F = new FacadeChess();
 }
 }
--- src/chess/FacadeChess.java
+++ src/chess/FacadeChess.java
@@ -18,6 +18,10 @@ public class FacadeChess {
         jFrame = new JFrame( "YetAnotherChessGame 1.0 -- ".concat(type) );
     }
 
+    FacadeChess() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     public void createGame() {
       
         jFrame = new JFrame( "YetAnotherChessGame 1.0" );
